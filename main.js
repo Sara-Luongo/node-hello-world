78f604c6dc4046b8a7ebea5c61811ece53dcf394
Sara-Luongo <saraluongo202002@gmail.com>
2026-05-20 16:56:31 +0200

    aggiunta anche una variabile al file env

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-console.log('hello world');
-console.log(process.argv)
+//console.log('hello world');
+//console.log('hello Boolean');
+//const parola = process.argv.slice(2);
+//console.log(`questa è la tua parola: ${parola}`)
+const segreto = process.env.PAROLA_SEGRETA
+console.log(segreto)
